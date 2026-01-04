6e8210ed35de2eb4d877425d6430b0dc099168b0
Nick Dombroski <nick.m.dombroski@gmail.com>
2026-01-04 13:15:07 -0500

    Move getFacePlaneForEdge to common library

---
+++ common/common.fs
@@ -0,0 +1,39 @@
+FeatureScript 2837;
+import(path : "onshape/std/common.fs", version : "2837.0");
+
+// This document contains functions for common operations shared between features
+
+/**
+ * Between all of the given bodies, find which face the given edge exists on
+ * and return a plane tangent to that face. This is used to automatically determine
+ * which plane a feature should be created on based off the selected edge and solid, 
+ * so that the user doesn't have to explicitly define it.
+ * Determines which body the edge is closest to and returns the tangent plane of that face.
+ * @param context : The context
+ * @param edge : Line/edge to use as reference
+ * @param bodies : Bodies to search for the closest face
+ * @return Plane : The tangent plane to use for extrusion
+*/
+export function getFacePlaneForEdge(context is Context, edge is Query, bodies is Query) returns Plane
+{
+    // To determine the plane to use for extruding, find the face of the given bodies
+    // that is closest to the first vertex of the given edge.
+    var edgeVertices = qAdjacent(edge, AdjacencyType.VERTEX, EntityType.VERTEX);
+    var verticesArray = evaluateQuery(context, edgeVertices);
+    var vertexPoint = evVertexPoint(context, { "vertex" : verticesArray[0] });
+    var closestFaces = qClosestTo(qOwnedByBody(bodies, EntityType.FACE), vertexPoint);
+    
+    if (isQueryEmpty(context, closestFaces))
+    {
+        throw regenError("No body found near the selected edge.");
+    }
+    
+    var closestFacesArray = evaluateQuery(context, closestFaces);
+    var face = closestFacesArray[0];
+    
+    // Get the tangent plane from the face to determine the extrusion direction
+    return evFaceTangentPlane(context, {
+            "face" : face,
+            "parameter" : vector(0.5, 0.5)
+    });
+}
--- tongueAndGroove/tongueAndGroove.fs
+++ tongueAndGroove/tongueAndGroove.fs
@@ -1,5 +1,6 @@
 FeatureScript 2837;
 import(path : "onshape/std/common.fs", version : "2837.0");
+Common::import(path : "91b3d7b1a713c22628315529", version : "7d3303174d0f08b441e2fc90");
 
 annotation { "Feature Type Name": "Tongue and Groove" }
 export const tongueAndGroove = defineFeature(function(context is Context, id is Id, definition is map)
@@ -124,7 +125,7 @@ export const tongueAndGroove = defineFeature(function(context is Context, id is 
 function createTongueBody(context is Context, id is Id, pathLine is Query, tongueParts is Query, tongueThickness is ValueWithUnits, tongueLength is ValueWithUnits, options is map) returns Query
 {
     // Get the tangent plane from a face of the tongue parts to determine the extrusion direction
-    var facePlane = getFacePlaneForEdge(context, pathLine, tongueParts);
+    var facePlane = Common::getFacePlaneForEdge(context, pathLine, tongueParts);
     
     // Extrude the line into a sheet body along the face normal
     var extrudeId = id + "extrude";
@@ -166,41 +167,6 @@ function createTongueBody(context is Context, id is Id, pathLine is Query, tongu
 
     // Return the thickened body
     return qCreatedBy(thickenId, EntityType.BODY);
-}
-
-/**
- * Between all of the given bodies, find which face the given edge exists on
- * and return a plane tangent to that face. This is used to automatically determine
- * which plane a feature should be created on based off the selected edge and solid, 
- * so that the user doesn't have to explicitly define it.
- * Determines which body the edge is closest to and returns the tangent plane of that face.
- * @param context : The context
- * @param edge : Line/edge to use as reference
- * @param bodies : Bodies to search for the closest face
- * @return Plane : The tangent plane to use for extrusion
-*/
-function getFacePlaneForEdge(context is Context, edge is Query, bodies is Query) returns Plane
-{
-    // To determine the plane to use for extruding, find the face of the given bodies
-    // that is closest to the first vertex of the given edge.
-    var edgeVertices = qAdjacent(edge, AdjacencyType.VERTEX, EntityType.VERTEX);
-    var verticesArray = evaluateQuery(context, edgeVertices);
-    var vertexPoint = evVertexPoint(context, { "vertex" : verticesArray[0] });
-    var closestFaces = qClosestTo(qOwnedByBody(bodies, EntityType.FACE), vertexPoint);
-    
-    if (isQueryEmpty(context, closestFaces))
-    {
-        throw regenError("No body found near the selected edge.");
-    }
-    
-    var closestFacesArray = evaluateQuery(context, closestFaces);
-    var face = closestFacesArray[0];
-    
-    // Get the tangent plane from the face to determine the extrusion direction
-    return evFaceTangentPlane(context, {
-            "face" : face,
-            "parameter" : vector(0.5, 0.5)
-    });
 }
 
 /**
